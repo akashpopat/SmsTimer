b82656539d699df929c7ef95df3c791f5a579afe
Pritesh <pripatel1712@gmail.com>
2016-05-10 11:21:35 +0530

    bug fixes

--- app/src/main/java/com/example/pritesh/smstimer/MainActivity.java
+++ app/src/main/java/com/example/pritesh/smstimer/MainActivity.java
@@ -4,16 +4,16 @@ package com.example.pritesh.smstimer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
+//import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.util.Log;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+//import android.util.Log;
+//import java.text.SimpleDateFormat;
+//import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private static final String tag="com.example.pritesh.smstimer";
